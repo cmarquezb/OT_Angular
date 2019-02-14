aff02dc1b2c23e0290a78b2dc58a429ed5aaf3c2
Cristhian Márquez <ctha.mb@gmail.com>
2019-02-14 13:32:50 -0300

    primero
    inicializado

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AreaListComponent } from './maestros/area-list/area-list.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    AreaListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
